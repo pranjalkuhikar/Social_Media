c4d9353fdf52158685c5ca7b3ae9ef96340e4877
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-30 13:20:47 +0530

    Refactor server configuration to use a dedicated config module for environment variables

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,7 @@
 import app from "./src/app.js";
-import dotenv from "dotenv";
-dotenv.config();
+import config from "./src/config/config.js";
 
-const port = process.env.PORT;
+const port = config.PORT;
 
 app.listen(port, () => {
   console.log(`Server is Running on ${port}`);
